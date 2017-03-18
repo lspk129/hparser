8ab85b5e7c5d2d997d934a2dc05b03d2eba62b93
Linas <l.spukas@gmail.com>
2017-03-18 22:17:38 +0000

    initial commit

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
-const express = require('express');
-const app = express();
-
-const port = port = process.env.PORT || 8080;
-
-app.get('/', (req, res) => {
-    const ip = req.headers['x-forwarded-for'];
-    const op = (/\(([^)]+)\)/.exec(req.headers['user-agent']))[1];
-    const lg = req.headers['accept-language'].split(',').slice(0, 1).join('');
+const express = require('express'); 
  
-    res.json({
-        ipaddress: ip,
-        language: lg,
-        software: op
-    });
-   
+const app = express(); 
+const PORT = process.env.PORT || 8080;
+ 
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
-
-app.listen(port, () => console.log('Connected to port ${port}'));
+ 
+app.listen(PORT, () => console.log('Server is listening on port ' + PORT));
