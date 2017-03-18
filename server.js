8968cf5a9fa0de8586619964eadcfee3cfab2e05
Linas <l.spukas@gmail.com>
2017-03-18 22:19:32 +0000

    initial commit

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = express();
 const PORT = process.env.PORT || 8080;
  
 app.get('/', (req, res) => {
-  res.send('Hello World');
+    const ip = req.headers['x-forwarded-for'];
+    res.send('Your IP address is: ' + ip);
 });
  
 app.listen(PORT, () => console.log('Server is listening on port ' + PORT));
