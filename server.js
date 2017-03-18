9c9917da1b1044ba1e80963b810e8aa40a6e9040
Linas <l.spukas@gmail.com>
2017-03-18 22:11:32 +0000

    Added project Head-parser

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require('express'); 
+const express = require('express');
+const app = express();
+
+const port = port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    const ip = req.headers['x-forwarded-for'];
+    const op = (/\(([^)]+)\)/.exec(req.headers['user-agent']))[1];
+    const lg = req.headers['accept-language'].split(',').slice(0, 1).join('');
  
-const app = express(); // This is how we initialize an express app
-const PORT = process.env.PORT || 8080;     // We set the port that the application will use
- 
-// When a GET request is made to the root path: '/' reply with Hello World
-app.get('/', (request, response) => {
-  response.send('Hello World');
+    res.json({
+        ipaddress: ip,
+        language: lg,
+        software: op
+    });
+   
 });
- 
-// We set the app to listen on the given PORT
-// It will log a message to the console once it is ready
-app.listen(PORT, () => console.log('Server is listening on port ' + PORT));
+
+app.listen(port, () => console.log('Connected to port ${port}'));
